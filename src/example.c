8d9d938831919f53a5a8f71d84bc5c7a50d4cf34
Liming SHAO <lmshao@163.com>
2018-06-06 23:09:29 +0800

    C template file

---
+++ src/example.c
@@ -0,0 +1,10 @@
+//
+// Created by Liming Shao on 2018/6/4.
+//
+
+#include "example.h"
+#include <stdio.h>
+
+void test_print(char *str) {
+    printf("\n===== example test ====== \n %s\n", str);
+}
---
+++ src/example.h
@@ -0,0 +1,10 @@
+//
+// Created by Liming Shao on 2018/6/4.
+//
+
+#ifndef EXAMPLE_H
+#define EXAMPLE_H
+
+void test_print(char *str);
+
+#endif //EXAMPLE_H
